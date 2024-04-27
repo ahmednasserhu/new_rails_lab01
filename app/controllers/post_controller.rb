436c05d1692138d753803c806869fc5feaa99c7b
ahmednasserhu <ahmedd.nasser.hussein@gmail.com>
2024-04-27 13:56:39 +0300

    Post controller crud operations added

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,3 +1,41 @@
-class PostController < ApplicationController
+class PostsController < ApplicationController
+  def index
+   @posts=Post.all
+  end
 
-end
+  def new
+    @post=Post.new
+  end
+
+ def create
+  @post = Post.new(title: params[:title], body: params[:body])
+   if @post.save
+    redirect_to @post
+   else
+    render :new
+   end
+  end
+
+  def show
+    @post=Post.find(params[:id])
+  end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+ def destroy
+  @post = post.find(params[:id])
+  @post.destroy
+  redirect_to @post
+ end
